5442ef9b894d04a000deb3a23f0ddecf0a829a8e
RaiA133 <rai010303@gmail.com>
2024-09-30 11:20:13 +0700

    feature: Install DaisyUI

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -5,13 +5,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
+    
   <link rel="stylesheet" href="<?= base_url(); ?>/src/output.css">
-</head>
+  </head>
 
-<body>
-  <h1 class="text-2xl font-bold underline">
-    Hello world!
-  </h1>
+<body data-theme='corporate' class="h-screen">
+
+<button class="btn">Button</button>
+
 </body>
 
 </html>
