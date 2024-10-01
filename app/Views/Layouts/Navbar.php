750d74dae4e8c05c800bb0ac878ad8b5c0dae8ea
RaiA133 <rai010303@gmail.com>
2024-10-01 09:27:42 +0700

    improvement: Sidebar kece & menu home improve

--- app/Views/Layouts/Navbar.php
+++ app/Views/Layouts/Navbar.php
@@ -1,48 +1,7 @@
 <!-- Navbar -->
 <div class="navbar bg-base-100">
   <div class="navbar-start">
-    <div class="dropdown">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h7" />
-        </svg>
-      </div>
-      <ul
-        tabindex="0"
-        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a href="<?= base_url() ?>">Home</a></li>
-        <li><a href="<?= base_url() ?>profil">Profil</a></li>
-        <li><a href="<?= base_url() ?>regulasi">Regulasi</a></li>
-      </ul>
-    </div>
-  </div>
-  <div class="navbar-center">
-    <a class="btn btn-ghost text-xl" href="<?= base_url() ?>">PPID BBSPJILM</a>
-  </div>
-  <div class="navbar-end">
-    <button class="btn btn-ghost btn-circle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-      </svg>
-    </button>
+    
     <button class="btn btn-ghost btn-circle">
       <div class="indicator">
         <svg
@@ -60,5 +19,63 @@
         <span class="badge badge-xs badge-primary indicator-item"></span>
       </div>
     </button>
+
+    <button class="btn btn-ghost btn-circle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+      </svg>
+    </button>
+    
+  </div>
+  <div class="navbar-center">
+    <a class="btn btn-ghost text-xl" href="<?= base_url() ?>">PPID BBSPJILM</a>
+  </div>
+  <div class="navbar-end">
+
+    <div class="drawer drawer-end flex justify-end">
+      <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content">
+        <!-- Page content here -->
+        <label tabindex="0" role="button" class="drawer-button btn btn-ghost btn-circle" for="my-drawer-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h7" />
+          </svg>
+        </label>
+      </div>
+      <div class="drawer-side z-50">
+        <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay" style="background-color: #0000;"></label>
+        <ul class="menu backdrop-blur-md bg-white/30 text-base-content min-h-full w-full md:w-4/12 p-4">
+          <!-- Sidebar content here -->
+          <label class="btn btn-sm btn-circle btn-ghost absolute pt-[1px] text-2xl font-light"for="my-drawer-4" aria-label="close sidebar">✕</label>
+          <div class="mt-32">
+            <div class="ml-4 my-3">001</div>
+            <li class=" text-4xl"><a href="<?= base_url() ?>">Home</a></li>
+            <div class="ml-4 my-3">002</div>
+            <li class=" text-4xl"><a href="<?= base_url() ?>profil">Profil</a></li>
+            <div class="ml-4 my-3">003</div>
+            <li class=" text-4xl"><a href="<?= base_url() ?>regulasi">Regulasi</a></li>
+          </div>
+        </ul>
+      </div>
+    </div>
+
   </div>
 </div>
--- app/Views/Pages/Home.php
+++ app/Views/Pages/Home.php
@@ -3,9 +3,60 @@
 
 <?php $this->section('content') ?>
 
-<div class="flex flex-col justify-center items-center">
+<div class="flex flex-col">
   <div class="text-2xl">Halaman Home</div>
   <div class="divider"></div>
+
+  <div class="flex flex-col gap-4">
+
+    <!-- <div class="card bg-base-100 w-96 shadow-xl">
+      <figure class="px-10 pt-10">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+          class="rounded-xl" />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions">
+          <button class="btn btn-neutral">Buy Now</button>
+        </div>
+      </div>
+    </div> -->
+
+    <div class="flex flex-col w-5/12 my-52">
+      <div class="flex items-center">
+        <div class="ml-4 my-3">001</div>
+        <div class="text-4xl ml-4"><a href="<?= base_url() ?>">Informasi Berkala</a></div>
+      </div>
+      <div class="mt-4 ml-4">
+        informasi berkala tentang kinerja, laporan keuangan, laporan kepegawaian, akses informasi publik, serta dokumen terkait kegiatan dan program instansi dari tahun 2015 hingga 2024.
+      </div>
+    </div>
+    
+    <div class="flex flex-col w-5/12 my-52">
+      <div class="flex items-center">
+        <div class="ml-4 my-3">002</div>
+        <div class="text-4xl ml-4"><a href="<?= base_url() ?>">Informasi Setiap Saat</a></div>
+      </div>
+      <div class="mt-4 ml-4">
+      informasi setiap saat terkait layanan BBSPJILM, jurnal, standar pelayanan, peraturan tarif, perjanjian kerja sama, serta prosedur penanganan keadaan darurat.
+      </div>
+    </div>
+
+    <div class="flex flex-col w-5/12 my-52">
+      <div class="flex items-center">
+        <div class="ml-4 my-3">003</div>
+        <div class="text-4xl ml-4"><a href="<?= base_url() ?>">Informai Sertamerta</a></div>
+      </div>
+      <div class="mt-4 ml-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur earum deserunt in. Ipsa maiores adipisci provident vel reiciendis! Praesentium perspiciatis nobis, beatae ut neque sunt cupiditate necessitatibus libero illum dolor.
+      </div>
+    </div>
+
+  </div>
+
 </div>
 
 <?php $this->endSection(); ?>
