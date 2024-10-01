ad274e090da65372535c1df2d4ff5e6e8cf39470
RaiA133 <rai010303@gmail.com>
2024-10-01 10:14:43 +0700

    feature: add Locomotive Scroll

--- app/Views/Pages/Home.php
+++ app/Views/Pages/Home.php
@@ -4,8 +4,10 @@
 <?php $this->section('content') ?>
 
 <div class="flex flex-col">
-  <div class="text-2xl">Halaman Home</div>
-  <div class="divider"></div>
+  <div>
+    <div class="text-2xl">Halaman Home</div>
+    <!-- <div class="divider"></div> -->
+  </div>
 
   <div class="flex flex-col gap-4">
 
@@ -30,8 +32,8 @@
         <div class="ml-4 my-3">001</div>
         <div class="text-4xl ml-4"><a href="<?= base_url() ?>">Informasi Berkala</a></div>
       </div>
-      <div class="mt-4 ml-4">
-        informasi berkala tentang kinerja, laporan keuangan, laporan kepegawaian, akses informasi publik, serta dokumen terkait kegiatan dan program instansi dari tahun 2015 hingga 2024.
+      <div class="mt-4 ml-4 text-justify">
+        informasi berkala tentang kinerja, laporan keuangan, laporan kepegawaian, akses informasi publik, serta dokumen terkait kegiatan dan program instansi dari tahun 2015 hingga Sekarang.
       </div>
     </div>
     
@@ -40,7 +42,7 @@
         <div class="ml-4 my-3">002</div>
         <div class="text-4xl ml-4"><a href="<?= base_url() ?>">Informasi Setiap Saat</a></div>
       </div>
-      <div class="mt-4 ml-4">
+      <div class="mt-4 ml-4 text-justify">
       informasi setiap saat terkait layanan BBSPJILM, jurnal, standar pelayanan, peraturan tarif, perjanjian kerja sama, serta prosedur penanganan keadaan darurat.
       </div>
     </div>
@@ -50,7 +52,7 @@
         <div class="ml-4 my-3">003</div>
         <div class="text-4xl ml-4"><a href="<?= base_url() ?>">Informai Sertamerta</a></div>
       </div>
-      <div class="mt-4 ml-4">
+      <div class="mt-4 ml-4 text-justify">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur earum deserunt in. Ipsa maiores adipisci provident vel reiciendis! Praesentium perspiciatis nobis, beatae ut neque sunt cupiditate necessitatibus libero illum dolor.
       </div>
     </div>
--- app/Views/Pages/Profil.php
+++ app/Views/Pages/Profil.php
@@ -3,7 +3,7 @@
 
 <?php $this->section('content') ?>
 
-<div class="flex flex-col justify-center items-center">
+<div class="flex flex-col">
   <div class="text-2xl">Halaman Profil</div>
   <div class="divider"></div>
 </div>
