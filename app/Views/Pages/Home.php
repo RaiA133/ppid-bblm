b6530d9ab2b15fa3fd0a8b2318b440347ec76bac
RaiA133 <rai010303@gmail.com>
2024-10-02 14:49:06 +0700

    feature: tampilan link pdf langsung di web

--- app/Views/Pages/Home.php
+++ app/Views/Pages/Home.php
@@ -4,55 +4,80 @@
 <?php $this->section('content') ?>
 
 <div class="flex flex-col" id="header-home">
-  <div data-scroll data-scroll-direction="horizontal" data-scroll-speed="-2" data-scroll-position="top" data-scroll-target="#header-home">
+
+  <!-- Judul Halaman -->
+  <section data-scroll data-scroll-direction="horizontal" data-scroll-speed="-2" data-scroll-position="top" data-scroll-target="#header-home">
     <div class="text-2xl">Halaman Home</div>
     <div class="divider"></div>
-  </div>
+  </section>
 
-  <div class="flex flex-col gap-4">
+  <!-- Informasi Utama -->
+  <section class="flex flex-col items-start md:flex-row" id="fixed-elements">
 
-    <div class="flex flex-col w-full sm:w-8/12 md:w-5/12 my-32" data-scroll data-scroll-speed="2">
-      <div class="flex items-center" data-scroll data-scroll-speed="1">
-        <div class="ml-4 my-3">001</div>
-        <a class="relative group  py-1.5 px-2.5 text-stone-900 text-4xl ml-4" href="<?= base_url() ?>">
-          <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
-          Informasi Berkala
-        </a>
+    <div class="flex flex-col gap-4">
+
+      <div class="flex flex-col w-full my-32" data-scroll data-scroll-speed="2">
+        <div class="flex items-center" data-scroll data-scroll-speed="1">
+          <div class="ml-0 md:ml-4 my-3">001</div>
+          <a class="relative group py-1.5 px-2.5 text-stone-900 text-4xl ml-4" href="<?= base_url() ?>">
+            <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+            Informasi Berkala
+          </a>
+        </div>
+        <div class="ml-0 md:ml-4 mt-4 text-justify" data-scroll data-scroll-speed="2">
+          informasi berkala tentang kinerja, laporan keuangan, laporan kepegawaian, akses informasi publik, serta dokumen terkait kegiatan dan program instansi dari tahun 2015 hingga Sekarang.
+        </div>
       </div>
-      <div class="mt-4 ml-4 text-justify" data-scroll data-scroll-speed="2">
-        informasi berkala tentang kinerja, laporan keuangan, laporan kepegawaian, akses informasi publik, serta dokumen terkait kegiatan dan program instansi dari tahun 2015 hingga Sekarang.
+
+      <div class="flex flex-col w-full my-32" data-scroll data-scroll-speed="3">
+        <div class="flex items-center" data-scroll data-scroll-speed="2">
+          <div class="ml-0 md:ml-4 my-3">002</div>
+          <a class="relative group py-1.5 px-2.5 text-stone-900 text-4xl ml-4" href="<?= base_url() ?>">
+            <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+            Informasi Setiap Saat
+          </a>
+        </div>
+        <div class="ml-0 md:ml-4 mt-4 text-justify" data-scroll data-scroll-speed="1">
+          informasi setiap saat terkait layanan BBSPJILM, jurnal, standar pelayanan, peraturan tarif, perjanjian kerja sama, serta prosedur penanganan keadaan darurat.
+        </div>
       </div>
+
+      <div class="flex flex-col w-full my-32" data-scroll data-scroll-speed="3">
+        <div class="flex items-center" data-scroll data-scroll-speed="1">
+          <div class="ml-0 md:ml-4 my-3">003</div>
+          <a class="relative group py-1.5 px-2.5 text-stone-900 text-4xl ml-4" href="<?= base_url() ?>">
+            <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+            Informai Sertamerta
+          </a>
+        </div>
+        <div class="ml-0 md:ml-4 mt-4 text-justify" data-scroll data-scroll-speed="2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur earum deserunt in. Ipsa maiores adipisci provident vel reiciendis! Praesentium perspiciatis nobis, beatae ut neque sunt cupiditate necessitatibus libero illum dolor.
+        </div>
+      </div>
+
     </div>
 
-    <div class="flex flex-col w-full sm:w-8/12 md:w-5/12 my-32" data-scroll data-scroll-speed="3">
-      <div class="flex items-center" data-scroll data-scroll-speed="2">
-        <div class="ml-4 my-3">002</div>
-        <a class="relative group  py-1.5 px-2.5 text-stone-900 text-4xl ml-4" href="<?= base_url() ?>">
-          <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
-          Informasi Setiap Saat
-        </a>
-      </div>
-      <div class="mt-4 ml-4 text-justify" data-scroll data-scroll-speed="1">
-        informasi setiap saat terkait layanan BBSPJILM, jurnal, standar pelayanan, peraturan tarif, perjanjian kerja sama, serta prosedur penanganan keadaan darurat.
-      </div>
+    <!-- Logo MIDC Animated -->
+    <div id="customHeight" style="height: 100vh;" class="" data-scroll data-scroll-sticky data-scroll-target="#fixed-elements">
+      <video src="<?= base_url() ?>/vid/animasi_logo_bblm.mp4" autoplay loop muted class=""></video>
     </div>
+    <script>
+      function adjustHeight() {
+        var div = document.getElementById('customHeight');
+        if (window.innerWidth <= 768) { // Jika lebar layar md atau lebih besar
+          div.style.height = '350px';
+        } else {
+          div.style.height = '100vh'; // Jika layar lebih kecil dari md
+        }
+      }
+      window.onload = adjustHeight;
+      window.onresize = adjustHeight;
+    </script>
 
-    <div class="flex flex-col w-full sm:w-8/12 md:w-5/12 my-32" data-scroll data-scroll-speed="3">
-      <div class="flex items-center" data-scroll data-scroll-speed="1">
-        <div class="ml-4 my-3">003</div>
-        <a class="relative group  py-1.5 px-2.5 text-stone-900 text-4xl ml-4" href="<?= base_url() ?>">
-          <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
-          Informai Sertamerta
-        </a>
-      </div>
-      <div class="mt-4 ml-4 text-justify" data-scroll data-scroll-speed="2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur earum deserunt in. Ipsa maiores adipisci provident vel reiciendis! Praesentium perspiciatis nobis, beatae ut neque sunt cupiditate necessitatibus libero illum dolor.
-      </div>
-    </div>
+  </section>
 
-  </div>
-
-  <div data-scroll class="relative h-screen mt-10">
+  <!-- Video Autoplay -->
+  <section data-scroll class="relative h-screen mt-32 md:mt-0">
     <video autoplay loop muted src="<?= base_url() ?>/vid/video_profile_bblm.mp4" class="absolute top-0 left-0 w-full h-full object-cover"></video>
     <div class="absolute inset-0 flex justify-center items-center text-center bg-stone-900 bg-opacity-50 px-10 xl:px-72">
       <div class="flex">
@@ -62,60 +87,55 @@
             <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-50 transition-all duration-300 group-hover:w-full"></span>
             Jelajahi
           </button>
-
           </span>
         </div>
-
       </div>
     </div>
-  </div>
+  </section>
 
-  <div class="flex flex-col md:flex-row gap-4 justify-end items-end mt-10">
+  <!-- Informasi lainnya -->
+  <sec class="flex flex-col md:flex-row gap-4 justify-center items-center mt-10">
 
-    <div class="flex flex-col w-full sm:w-8/12 md:w-5/12 my-32" data-scroll data-scroll-speed="1">
+    <div class="flex flex-col w-full sm:w-8/12 md:w-5/12 my-10" data-scroll data-scroll-speed="1">
       <div class="flex items-center">
-        <div class="ml-4 my-3">004</div>
-        <a class="relative group  py-1.5 px-2.5 text-stone-900 text-3xl ml-4" href="<?= base_url() ?>">
+        <div class="ml-0 md:ml-4 my-3">004</div>
+        <a class="relative group py-1.5 px-2.5 text-stone-900 text-3xl ml-4" href="<?= base_url() ?>">
           <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
           Layanan informasi
         </a>
       </div>
-      <div class="mt-4 ml-4 text-justify">
+      <div class="ml-0 md:ml-4 mt-4 text-justify">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, eum numquam necessitatibus ab, minima voluptate quis porro quaerat optio soluta cupiditate, consequatur consectetur! Sunt obcaecati ea aut, quaerat fuga sed.
       </div>
     </div>
 
-    <div class="flex flex-col w-full sm:w-8/12 md:w-5/12 my-32" data-scroll data-scroll-speed="3">
+    <div class="flex flex-col w-full sm:w-8/12 md:w-5/12 my-10" data-scroll data-scroll-speed="3">
       <div class="flex items-center">
-        <div class="ml-4 my-3">005</div>
-        <a class="relative group  py-1.5 px-2.5 text-stone-900 text-3xl ml-4" href="<?= base_url() ?>">
+        <div class="ml-0 md:ml-4 my-3">005</div>
+        <a class="relative group py-1.5 px-2.5 text-stone-900 text-3xl ml-4" href="<?= base_url() ?>">
           <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
           Unit Pelayanan Publik
         </a>
       </div>
-      <div class="mt-4 ml-4 text-justify">
+      <div class="ml-0 md:ml-4 mt-4 text-justify">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ullam cum tempora sapiente nam earum odio eos, eaque quod magnam voluptatem amet molestiae eum! Aspernatur optio et sint ab animi.
       </div>
     </div>
 
-    <div class="flex flex-col w-full sm:w-8/12 md:w-5/12 my-32" data-scroll data-scroll-speed="5">
+    <div class="flex flex-col w-full sm:w-8/12 md:w-5/12 my-10" data-scroll data-scroll-speed="5">
       <div class="flex items-center">
-        <div class="ml-4 my-3">006</div>
-        <!-- <div class="text-3xl ml-4"><a href="<?= base_url() ?>">Laporan Layanan Informasi</a></div> -->
-        <a class="relative group  py-1.5 px-2.5 text-stone-900 text-3xl ml-4" href="<?= base_url() ?>">
+        <div class="ml-0 md:ml-4 my-3">006</div>
+        <a class="relative group py-1.5 px-2.5 text-stone-900 text-3xl ml-4" href="<?= base_url() ?>">
           <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
           Laporan Layanan Informasi
         </a>
       </div>
-      <div class="mt-4 ml-4 text-justify">
+      <div class="ml-0 md:ml-4 mt-4 text-justify">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur earum deserunt in. Ipsa maiores adipisci provident vel reiciendis! Praesentium perspiciatis nobis, beatae ut neque sunt cupiditate necessitatibus libero illum dolor.
       </div>
     </div>
 
-  </div>
-
-
-
+  </sec>
 
 </div>
 
--- app/Views/Pages/Regulasi.php
+++ app/Views/Pages/Regulasi.php
@@ -3,9 +3,131 @@
 
 <?php $this->section('content') ?>
 
-<div class="flex flex-col" id="header-regulasi">
-  <div class="text-2xl" data-scroll data-scroll-direction="horizontal" data-scroll-speed="-2" data-scroll-position="top" data-scroll-target="#header-regulasi">Halaman Regulasi</div>
-  <div class="divider"></div>
+<div class="flex flex-col">
+
+  <!-- Judul Halaman -->
+  <section class="flex flex-col" id="header-regulasi">
+    <div class="text-2xl" data-scroll data-scroll-direction="horizontal" data-scroll-speed="-2" data-scroll-position="top" data-scroll-target="#header-regulasi">Halaman Regulasi</div>
+    <div class="divider"></div>
+  </section>
+
+  <!-- List Link Regulasi -->
+  <section data-scroll-section>
+    <div class="">
+      <div class="" data-scroll>
+        <div class="flex flex-col md:flex-row gap-4">
+
+          <div class="basis-1/2 lg:basis-1/4 border w-full">
+            <p class="p-10">
+              Dalam upaya meningkatkan transparansi dan akuntabilitas, beberapa peraturan telah ditetapkan untuk mendukung Keterbukaan Informasi Publik. Berikut adalah beberapa regulasi penting beserta penjelasan singkatnya:
+            </p>
+          </div>
+
+          <div class="basis-3/4 border w-full p-10">
+
+            <div class="join join-vertical w-full">
+
+              <div class="collapse collapse-arrow join-item border-base-300 border group">
+                <input type="radio" name="my-accordion-4"  />
+                <div class="collapse-title text-xl font-medium relative">
+                  <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+                  01. UU No 14 Tahun 2008, Tentang Keterbukaan Informasi Publik
+                </div>
+                <div class="collapse-content">
+                  <p class="mt-4">
+                    <iframe src="https://drive.google.com/file/d/1QIbpVc2YsZmHMoVerxxAe5FbwsTUhVWu/preview" width="100%" height="600px"></iframe>
+                  </p>
+                </div>
+              </div>
+
+              <div class="collapse collapse-arrow join-item border-base-300 border group">
+                <input type="radio" name="my-accordion-4" />
+                <div class="collapse-title text-xl font-medium relative">
+                  <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+                  02. PP No 61 Tahun 2010, Tentang Pelaksanaan Undang – Undang Keterbukaan Informasi Publik
+                </div>
+                <div class="collapse-content">
+                  <p class="mt-4">
+                    <iframe src="https://drive.google.com/file/d/1KefQXXB9d0uI3H3frBhvkcIUT6r1LE6D/preview" width="100%" height="600px"></iframe>
+                  </p>
+                </div>
+              </div>
+
+              <div class="collapse collapse-arrow join-item border-base-300 border group">
+                <input type="radio" name="my-accordion-4" />
+                <div class="collapse-title text-xl font-medium relative">
+                  <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+                  03. Perkip No 1 Tahun 2010, Tentang Standar Layanan Informasi Publik
+                </div>
+                <div class="collapse-content">
+                  <p class="mt-4">
+                    <iframe src="https://drive.google.com/file/d/1CwCkVDTwnyODyge-Wcft_XhvkL8wTwUQ/preview" width="100%" height="600px"></iframe>
+                  </p>
+                </div>
+              </div>
+
+              <div class="collapse collapse-arrow join-item border-base-300 border group">
+                <input type="radio" name="my-accordion-4" />
+                <div class="collapse-title text-xl font-medium relative">
+                  <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+                  04. Perkip No 1 Tahun 2013, Tentang Prosedur Penyelesaian Sengketa Informasi Publik
+                </div>
+                <div class="collapse-content">
+                  <p class="mt-4">
+                    <iframe src="https://drive.google.com/file/d/17OqqZMwI5H1n8Wiy6QS-R_RgRLs5fs3y/preview" width="100%" height="600px"></iframe>
+                  </p>
+                </div>
+              </div>
+
+              <div class="collapse collapse-arrow join-item border-base-300 border group">
+                <input type="radio" name="my-accordion-4" />
+                <div class="collapse-title text-xl font-medium relative">
+                  <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+                  05. Perma No 2 Tahun 2011, Tentang Tata Cara Penyelesaian Sengketa Informasi Publik di Pengadilan
+                </div>
+                <div class="collapse-content">
+                  <p class="mt-4">
+                    <iframe src="https://drive.google.com/file/d/1fzZ8UsjZUv9U72RKwjbx8j2mxXp28Ovz/preview" width="100%" height="600px"></iframe>
+                  </p>
+                </div>
+              </div>
+
+              <div class="collapse collapse-arrow join-item border-base-300 border group">
+                <input type="radio" name="my-accordion-4" />
+                <div class="collapse-title text-xl font-medium relative">
+                  <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+                  06. KMA No 085 Tahun 2011, Tentang Pembentukan Kelompok Kerja Penyusunan Peraturan Mahkamah Agung tentang Tata Cara Penyelesaian Gugatan Atas Putusan Komisi Informasi Pada Pengadilan Tata Usaha Negara Dan / Atau Pengadilan Negeri
+                </div>
+                <div class="collapse-content">
+                  <p class="mt-4">
+                    <iframe src="https://drive.google.com/file/d/15006kMyutfeIZP2G3BBdPgHWUmwVo6NQ/preview" width="100%" height="600px"></iframe>
+                  </p>
+                </div>
+              </div>
+
+              <div class="collapse collapse-arrow join-item border-base-300 border group">
+                <input type="radio" name="my-accordion-4" />
+                <div class="collapse-title text-xl font-medium relative">
+                  <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-stone-900 transition-all duration-300 group-hover:w-full"></span>
+                  07. Permenperin Nomor 1 Tahun 2022 Tentang Organisasi dan Tata Kerja Unit Pelaksana Teknis di Lingkungan Badan Standardisasi dan Kebijakan Jasa Industri
+                </div>
+                <div class="collapse-content">
+                  <p class="mt-4">
+                    <iframe src="https://drive.google.com/file/d/1nx1AXkC6GOEYwzR7IZ_c_E1pVEBVmtO6/preview" width="100%" height="600px"></iframe>
+                  </p>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
 </div>
+
 
 <?php $this->endSection(); ?>
