9df84bb27c1871792883045b1a2cf941a25866fb
RaiA133 <rai010303@gmail.com>
2024-10-08 11:33:44 +0700

    fix: tampilan Login admin

--- app/Views/Pages/Admin/Auth/Login.php
+++ app/Views/Pages/Admin/Auth/Login.php
@@ -1,52 +1,78 @@
-<?php $this->extend('Layouts/Template'); ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PPID | BBLM</title>
+  <link rel="stylesheet" href="<?= base_url() ?>src/output.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/locomotive-scroll/dist/locomotive-scroll.css">
+</head>
 
-<?php $this->section('content') ?>
+<body data-theme="corporate" class="flex">
 
-<div class="flex flex-col justify-center items-center w-full">
+  <div class="w-full">
+    <div data-scroll-container>
 
-  <div class="card mx-auto w-full max-w-5xl shadow-xl">
-    <div class="grid md:grid-cols-2 grid-cols-1 bg-base-100 rounded-xl">
-      <div class="">
-        <!-- Ganti ini dengan konten yang diinginkan seperti gambar atau teks intro -->
-        <img src="https://via.placeholder.com/500x400" alt="Landing Intro" class="w-full h-full object-cover rounded-l-xl">
-      </div>
-      <div class="py-24 px-10">
-        <h2 class="text-2xl font-semibold mb-2 text-center">Login</h2>
-        <form action="/login" method="POST">
-          <div class="mb-4">
-            <!-- Input Email -->
-            <div class="mt-4">
-              <label for="emailId" class="block text-sm font-medium text-gray-700">Email Id</label>
-              <input type="email" name="emailId" id="emailId" class="input input-bordered w-full mt-2" placeholder="Enter your email" required>
+      <div class="flex flex-col justify-center items-center w-full h-screen">
+
+        <div class="card mx-auto w-full max-w-5xl shadow-xl">
+          <div class="grid md:grid-cols-2 grid-cols-1 bg-base-100 rounded-xl">
+            <div class="">
+              <!-- Ganti ini dengan konten yang diinginkan seperti gambar atau teks intro -->
+              <img src="https://via.placeholder.com/500x400" alt="Landing Intro" class="w-full h-full object-cover rounded-l-xl">
             </div>
+            <div class="py-24 px-10">
+              <h2 class="text-2xl font-semibold mb-2 text-center">Login</h2>
+              <form action="/login" method="POST">
+                <div class="mb-4">
+                  <!-- Input Email -->
+                  <div class="mt-4">
+                    <label for="emailId" class="block text-sm font-medium text-gray-700">Email Id</label>
+                    <input type="email" name="emailId" id="emailId" class="input input-bordered w-full mt-2" placeholder="Enter your email" required>
+                  </div>
 
-            <!-- Input Password -->
-            <div class="mt-4">
-              <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-              <input type="password" name="password" id="password" class="input input-bordered w-full mt-2" placeholder="Enter your password" required>
+                  <!-- Input Password -->
+                  <div class="mt-4">
+                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                    <input type="password" name="password" id="password" class="input input-bordered w-full mt-2" placeholder="Enter your password" required>
+                  </div>
+                </div>
+
+                <div class="text-right text-primary">
+                  <a href="/forgot-password" class="text-sm inline-block hover:text-primary hover:underline transition duration-200">Forgot Password?</a>
+                </div>
+
+                <!-- Error Message Placeholder -->
+                <p class="text-red-500 mt-8" id="errorMessage"></p>
+
+                <!-- Login Button -->
+                <button type="submit" class="btn mt-2 w-full btn-primary">Login</button>
+
+                <div class="text-center mt-4">
+                  Don't have an account yet?
+                  <a href="/register" class="inline-block hover:text-primary hover:underline transition duration-200">Register</a>
+                </div>
+              </form>
             </div>
           </div>
+        </div>
 
-          <div class="text-right text-primary">
-            <a href="/forgot-password" class="text-sm inline-block hover:text-primary hover:underline transition duration-200">Forgot Password?</a>
-          </div>
-
-          <!-- Error Message Placeholder -->
-          <p class="text-red-500 mt-8" id="errorMessage"></p>
-
-          <!-- Login Button -->
-          <button type="submit" class="btn mt-2 w-full btn-primary">Login</button>
-
-          <div class="text-center mt-4">
-            Don't have an account yet?
-            <a href="/register" class="inline-block hover:text-primary hover:underline transition duration-200">Register</a>
-          </div>
-        </form>
       </div>
+
     </div>
   </div>
 
-</div>
+  <script src="https://cdn.jsdelivr.net/npm/locomotive-scroll/dist/locomotive-scroll.js"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true
+    });
+  </script>
 
-<?php $this->endSection(); ?>
+
+</body>
+
+</html>
