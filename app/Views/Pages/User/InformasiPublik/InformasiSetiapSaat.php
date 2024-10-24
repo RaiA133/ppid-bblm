82c4377a0b0443cec086abffd6ff49855de3455b
RaiA133 <rai010303@gmail.com>
2024-10-24 12:30:58 +0700

    bugfix: ubah proses query untuk tampilan front informasi berkala dan setiap saat serta perubahan logic di informsi setiap saat front

--- app/Views/Pages/User/InformasiPublik/InformasiSetiapSaat.php
+++ app/Views/Pages/User/InformasiPublik/InformasiSetiapSaat.php
@@ -1,9 +1,6 @@
 <?php $this->extend('Layouts/Template'); ?>
 
-
 <?php $this->section('content') ?>
-
-
 
 <section class="flex flex-col mx-10" id="header-profil">
   <div class="text-2xl" data-scroll data-scroll-direction="horizontal" data-scroll-speed="-2" data-scroll-position="top" data-scroll-target="#header-profil">Informasi Setiap Saat</div>
@@ -13,6 +10,7 @@
 <section class="mx-10 mb-10 flex justify-center" data-scroll data-scroll-speed="5">
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4 w-full justify-center">
 
+    <!-- Iterasi melalui judul informasi setiap saat -->
     <?php foreach ($judulInformasiSetiapSaat as $judul) : ?>
       <div class="card h-fit mb-3 bg-neutral rounded-xl">
         <ul class="menu max-w-full shadow-lg text-neutral-content">
@@ -21,11 +19,16 @@
           <div class="divider before:bg-primary after:bg-secondary my-0"></div>
 
           <!-- Iterasi melalui informasi yang sesuai dengan judul saat ini -->
-          <?php foreach ($informasiSetiapSaat as $informasi) : ?>
-            <?php if ($informasi['id_informasi_setiap_saat_judul'] == $judul['id_informasi_setiap_saat_judul']) : ?>
-              <!-- Menampilkan list informasi yang sesuai -->
-              <li class="hover:bg-gray-950"><a target="_blank" href="<?= $informasi['informasi'] ?>"><?= $informasi['jenis_informasi'] ?></a></li>
-            <?php endif; ?>
+          <?php 
+          // Menyaring informasi yang sesuai dengan id judul
+          $filteredInformasi = array_filter($informasiSetiapSaat, function($informasi) use ($judul) {
+              return $informasi['id_informasi_setiap_saat_judul'] == $judul['id_informasi_setiap_saat_judul'];
+          });
+          ?>
+
+          <!-- Menampilkan list informasi yang sesuai -->
+          <?php foreach ($filteredInformasi as $informasi) : ?>
+            <li class="hover:bg-gray-950"><a target="_blank" href="<?= $informasi['informasi'] ?>"><?= $informasi['jenis_informasi'] ?></a></li>
           <?php endforeach; ?>
 
         </ul>
