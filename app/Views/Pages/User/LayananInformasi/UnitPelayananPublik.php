1d2ff4d1c1029ebd043fe673d2170b8ad8901a40
muhammadfija <muhammadfija120402@gmail.com>
2024-11-05 08:59:03 +0700

    bugfix: update responsive standarLayanan, layananInformasi and informasiPublik

--- app/Views/Pages/User/LayananInformasi/UnitPelayananPublik.php
+++ app/Views/Pages/User/LayananInformasi/UnitPelayananPublik.php
@@ -10,13 +10,13 @@
 
     <div class="flex flex-col sm:flex-row justify-center w-full gap-4 mt-20">
 
-      <div id="customHeight" class="h-fit" data-scroll data-scroll-sticky data-scroll-target="#fixed-elements">
+      <div class="h-fit w-fit sm:max-w-[50vw]" data-scroll data-scroll-sticky data-scroll-target="#fixed-elements">
         <div class="card border w-full shadow-xl rounded-xl">
           <figure class="px-4 pt-4">
             <img
               src="<?= base_url() ?>img/layananInformasi/unitPelayananPublik/<?= $results['link_gambar'] ?>"
               alt="Unit Pelayanan Publik"
-              class="rounded-xl bg-base-200 border" />
+              class="rounded-xl bg-base-200 border " />
           </figure>
           <div class="py-5 pr-5 text-end self-end w-1/2">
             <h2 class="text-xl text-indigo-500 font-extrabold">UNIT PELAYANAN PUBLIK</h2>
@@ -25,7 +25,7 @@
       </div>
 
       <!-- Konten di Samping Gambar -->
-      <div id="customHeight" class="h-fit sm:w-1/2" data-scroll data-scroll-sticky data-scroll-target="#fixed-elements">
+      <div class="h-fit sm:w-1/2 sm:max-w-[50vw]" data-scroll data-scroll-sticky data-scroll-target="#fixed-elements">
         <div class="w-full max-w-2xl shadow-xl rounded-xl">
           <div class="w-full py-10 px-10 rounded-xl border">
             <article class="prose prose-sm prose-headings:text-indigo-500 max-w-full">
