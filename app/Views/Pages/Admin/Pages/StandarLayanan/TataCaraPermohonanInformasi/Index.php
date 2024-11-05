93f7aed222eb9670e4db481300821d755a5bba03
muhammadfija <muhammadfija120402@gmail.com>
2024-11-05 10:10:27 +0700

    bugfix: update responsive admin standarLayanan, layananInformasi and informasiPublik

--- app/Views/Pages/Admin/Pages/StandarLayanan/TataCaraPermohonanInformasi/Index.php
+++ app/Views/Pages/Admin/Pages/StandarLayanan/TataCaraPermohonanInformasi/Index.php
@@ -69,79 +69,80 @@
                   </div>
                 </div>
               </div>
+            </div>
 
-            <?php else : ?>
-              <!-- Jika Hanya Gambar atau Hanya Konten -->
-              <div class="flex justify-center w-full mt-20">
-                <div data-scroll-offset class="w-fit max-w-2xl text-start border shadow-xl rounded-xl">
+          <?php else : ?>
+            <!-- Jika Hanya Gambar atau Hanya Konten -->
+            <div class="flex justify-center w-full mt-20">
+              <div data-scroll-offset class="w-fit max-w-2xl text-start border shadow-xl rounded-xl">
 
-                  <?php if (!empty($results['link_gambar'])) : ?>
-                    <div class="card w-full">
-                      <figure class="px-4 pt-4">
-                        <img
-                          src="<?= base_url() ?>img/standarLayanan/tataCaraPermohonanInformasi/<?= $results['link_gambar'] ?>"
-                          alt="Tata Cara Permohonan Informasi"
-                          class="rounded-xl bg-base-200 border" />
-                      </figure>
-                      <div class="py-5 pr-5 text-end self-end w-1/2">
-                        <h2 class="text-xl text-indigo-500 font-extrabold">TATA CARA PERMOHONAN INFORMASI</h2>
-                      </div>
+                <?php if (!empty($results['link_gambar'])) : ?>
+                  <div class="card w-full">
+                    <figure class="px-4 pt-4">
+                      <img
+                        src="<?= base_url() ?>img/standarLayanan/tataCaraPermohonanInformasi/<?= $results['link_gambar'] ?>"
+                        alt="Tata Cara Permohonan Informasi"
+                        class="rounded-xl bg-base-200 border" />
+                    </figure>
+                    <div class="py-5 pr-5 text-end self-end w-1/2">
+                      <h2 class="text-xl text-indigo-500 font-extrabold">TATA CARA PERMOHONAN INFORMASI</h2>
                     </div>
-                  <?php endif; ?>
+                  </div>
+                <?php endif; ?>
 
-                  <?php if (!empty($results['content'])) : ?>
-                    <div class="w-full max-w-2xl">
-                      <div class="w-full py-8 px-10">
-                        <article class="prose prose-sm prose-headings:text-indigo-500" id="content-preview-tata-cara-permohonan-informasi">
-                          <?= $results['content'] ?>
-                        </article>
-                        <div class="divider"></div>
-                        <h2 class="text-xl text-indigo-500 font-extrabold mt-5 text-end">TATA CARA PERMOHONAN INFORMASI</h2>
-                      </div>
+                <?php if (!empty($results['content'])) : ?>
+                  <div class="w-full max-w-2xl">
+                    <div class="w-full py-8 px-10">
+                      <article class="prose prose-sm prose-headings:text-indigo-500" id="content-preview-tata-cara-permohonan-informasi">
+                        <?= $results['content'] ?>
+                      </article>
+                      <div class="divider"></div>
+                      <h2 class="text-xl text-indigo-500 font-extrabold mt-5 text-end">TATA CARA PERMOHONAN INFORMASI</h2>
                     </div>
+                  </div>
 
-                  <?php endif; ?>
+                <?php endif; ?>
 
-                </div>
               </div>
-            <?php endif; ?>
+            </div>
+          <?php endif; ?>
 
-            <script>
-              let isAttributesRemoved = false; // Status apakah atribut telah dihapus
-              let isAttributesAdded = false; // Status apakah atribut telah ditambahkan
+          <script>
+            let isAttributesRemoved = false; // Status apakah atribut telah dihapus
+            let isAttributesAdded = false; // Status apakah atribut telah ditambahkan
 
-              function adjustAttributes() {
-                const div = document.getElementById('customHeight');
+            function adjustAttributes() {
+              const div = document.getElementById('customHeight');
 
-                if (window.innerWidth <= 768 && !isAttributesRemoved) {
-                  console.log('hapus');
-                  div.removeAttribute('data-scroll');
-                  div.removeAttribute('data-scroll-sticky');
-                  div.removeAttribute('data-scroll-target');
-                  isAttributesRemoved = true;
-                  isAttributesAdded = false;
-                } else if (window.innerWidth > 768 && !isAttributesAdded) {
-                  console.log('ada');
-                  div.setAttribute('data-scroll', '');
-                  div.setAttribute('data-scroll-sticky', '');
-                  div.setAttribute('data-scroll-target', '#fixed-elements');
-                  isAttributesAdded = true;
-                  isAttributesRemoved = false;
-                }
+              if (window.innerWidth <= 768 && !isAttributesRemoved) {
+                console.log('hapus');
+                div.removeAttribute('data-scroll');
+                div.removeAttribute('data-scroll-sticky');
+                div.removeAttribute('data-scroll-target');
+                isAttributesRemoved = true;
+                isAttributesAdded = false;
+              } else if (window.innerWidth > 768 && !isAttributesAdded) {
+                console.log('ada');
+                div.setAttribute('data-scroll', '');
+                div.setAttribute('data-scroll-sticky', '');
+                div.setAttribute('data-scroll-target', '#fixed-elements');
+                isAttributesAdded = true;
+                isAttributesRemoved = false;
               }
+            }
 
-              // Menggunakan addEventListener untuk menangani perubahan ukuran jendela
-              window.addEventListener('resize', (event) => {
-                adjustAttributes();
-              });
-
-              // Menggunakan onresize untuk menangani perubahan ukuran jendela
-              window.onresize = (event) => {
-                adjustAttributes();
-              };
-
+            // Menggunakan addEventListener untuk menangani perubahan ukuran jendela
+            window.addEventListener('resize', (event) => {
               adjustAttributes();
-            </script>
+            });
+
+            // Menggunakan onresize untuk menangani perubahan ukuran jendela
+            window.onresize = (event) => {
+              adjustAttributes();
+            };
+
+            adjustAttributes();
+          </script>
         </section>
 
       </div>
@@ -165,7 +166,7 @@
 
     <!-- Title & Edit Button -->
     <div class="flex justify-between items-center w-full gap-3">
-      <div class="text-md sm:text-xl font-bold">Edit Data Standar Tata cara Permohonan Informasi</div>
+      <div class="text-md sm:text-xl font-bold">Edit Data Tata Cara permohonan Informasi</div>
       <button type="submit" class="btn w-32 btn-neutral">Edit</button>
     </div>
 
